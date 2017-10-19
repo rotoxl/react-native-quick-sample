a9516005f521671ecd100dbca2810fbf33578bd8
ermolina <ermolina@e-externas.aena.es>
2017-10-19 12:59:55 +0200

    Several circles and ovals added, each one with a related color and rotating in a different direction

--- app/components/ColorCircle.js
+++ app/components/ColorCircle.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
-import {Dimensions, StyleSheet} from "react-native";
+import {Dimensions, StyleSheet, View} from "react-native";
 
 import * as Animatable from "react-native-animatable";
+const Color = require("pigment/full");
 
 export default class ColorCircle extends Component {
 
@@ -20,7 +21,7 @@ export default class ColorCircle extends Component {
 
     componentDidUpdate(prevProps, prevState) {
 
-        this.refs.circle.rubberBand(400)
+        this.refs.circlescontainer.rubberBand(1000)
     }
 
     render() {
@@ -34,11 +35,43 @@ export default class ColorCircle extends Component {
             ratio = height * 1.1;
         }
 
+        var c = new Color(this.props.color)
+        let ratio2=ratio-20*Math.random()
+        let ratio3=ratio+20*Math.random()
+        let ratio4=ratio+20*Math.random()
+
         return (
-            <Animatable.View
-                ref="circle"
-                style={[styles.circle, {borderColor: this.props.color, height: ratio, width: ratio, borderRadius: ratio/2}]}>
-                {this.props.children}
+            <Animatable.View ref="circlescontainer" style={{position:'absolute', paddingTop:60, top:0, bottom:0,  }}>
+                <Animatable.View
+                    ref="circle"
+                    style={[styles.circle, {borderColor:this.props.color, height: ratio, width: ratio, borderRadius: ratio/2}]}>
+                    {this.props.children}
+                </Animatable.View>
+
+                <Animatable.View
+                    ref="circle2"
+                    duration={Math.max(20000*Math.random(), 8000)}
+                    animation="rotate"
+                    iterationCount="infinite"
+                    style={[styles.circle, { position:'absolute', left:5*Math.random(), top:+10*Math.random(), borderWidth:15*Math.random(), borderColor:c.lighten(.3).tohex(), height: ratio2+42, width: ratio2, borderRadius: ratio2/2}]}>
+                </Animatable.View>
+                <Animatable.View
+                    ref="circle3"
+                    duration={Math.max(20000*Math.random(), 8000)}
+                    direction="reverse"
+                    animation="rotate"
+                    iterationCount="infinite"
+                    style={[styles.circle, {position:'absolute', left:-5*Math.random(), top:-10*Math.random(), borderWidth:20*Math.random(), borderColor:c.lighten(.6).tohex(), height: ratio3+40, width: ratio3-40, borderRadius: ratio3/2}]}>
+                </Animatable.View>
+                
+                <Animatable.View
+                    ref="circle4"
+                    duration={Math.max(20000*Math.random(), 8000)}
+                    direction="alternate"
+                    animation="rotate"
+                    iterationCount="infinite"                     
+                    style={[styles.circle, {position:'absolute', left:+10*Math.random(), top:+5*Math.random(), borderWidth:5*Math.random(), borderColor:c.darken(.2).tohex(), height: ratio4-40, width: ratio4+10, borderRadius: ratio4/2}]}>
+                </Animatable.View>
             </Animatable.View>
         );
     }
@@ -46,11 +79,9 @@ export default class ColorCircle extends Component {
 
 
 const styles = StyleSheet.create({
-
     circle: {
-        borderWidth: 10,
+        borderWidth: 30,
         alignItems: "center",
         justifyContent: "center",
-        backgroundColor: "#e8e8e8"
     },
 });
